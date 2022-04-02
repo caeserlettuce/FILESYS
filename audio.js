0eea2c96d59e43b95be4035554bd2e9b292e3efa
caeserlettuce <caeserlettuce@gmail.com>
2022-04-02 15:42:43 -0700

    audio not working but maybe soon (?)

---
+++ audio.js
@@ -0,0 +1,9 @@
+// for audio handling
+
+var aud_background = document.getElementById("aud_background");
+var aud_keyboard_1 = document.getElementById("aud_keyboard_1");
+var aud_keyboard_2 = document.getElementById("aud_keyboard_2");
+var aud_beep = document.getElementById("aud_beep");
+
+
+aud_keyboard_2.play();
--- setup.js
+++ setup.js
@@ -1,3 +1,63 @@
 // this is for important info about this thing, these variables get launched before any other
 
 var version = "v0.0.01a";
+
+
+function deb(message, prefix) {
+    var fullPrefix = "";
+    if (prefix) {
+        fullPrefix = `[${prefix.toUpperCase()}]: `;
+    } else {
+        fullPrefix = "";
+    }
+    console.log(`${fullPrefix}${message}`);
+}
+
+function debi(message, prefix) {
+    if (debug == true) {
+        deb(message, prefix);
+    }
+}
+
+
+function keyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+}
+
+function checkLen(value, len) {
+    var in_len = value.length;
+    var final = "";
+    if (in_len == len) {                      // if length of file tree is exactly the width of the space
+        final = `${value}`;
+    } else if (in_len < len) {                // if the file tree is shorter than the width of the space
+        var diff = len - in_len;
+        final = `${value}${" ".repeat(diff)}`;
+    } else if (in_len > len) {                // if the file tree is wider than the width of the space
+        var diff = in_len - len;
+        final = `...${value.substring(diff + 3)}`;
+    }
+    return final;
+}
+
+function arrDim(arr) {
+    return [ arr.length, arr[0].length ];
+}
+
+
+function copyArr(array) {
+    return [...array]
+}
+
+function parseToEval(array) {
+    var finalJsonPath = ``;                                 // setup variable
+    for (i in array) {                                     // for i in path segments
+        finalJsonPath = `${finalJsonPath}["${array[i]}"]`; // add it to a string version of json notation
+    }
+    //deb(`getting contents of path ${finalJsonPath}`, 'indexdir');
+    return finalJsonPath
+}
+
+function* times(x) {
+    for (var i = 0; i < x; i++)
+        yield i;
+}
